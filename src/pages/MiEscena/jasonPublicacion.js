47618e7631af00766ef411bcf53adfbbe1b5aa25
marksalazarx <marksalazarx@gmail.com>
2024-05-04 10:27:26 -0600

    Sección nueva publicación y modales asociados

---
+++ src/pages/MiEscena/jasonPublicacion.js
@@ -0,0 +1,16 @@
+const crearPublicacionTextoMultimedia = {
+    textoPublicacion, // Texto
+    archivoMultimedia, // Opcional, puede o no, enviar foto ni vídeo
+    }
+    
+    const crearPublicacionBusquedaArtistasOEscenarios = {
+    tituloPublicacion, // String,
+    textoPublicacion, // String,
+    tipoDeBusqueda, // Radiobutton: Artista o Escenario
+    generoMusical, // Checkbox con opciones de generos
+    instrumentos, // Checkbox con opciones de instrumentos
+    codigoPostal, // Número
+    ciudadOMunicipio, // Texto
+    entidadFederativa, // Texto
+    imagen, // Opcional
+    }
