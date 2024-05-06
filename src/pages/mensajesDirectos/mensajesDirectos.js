f89ec66807a1bf141e7a189e218dc8ad45f97807
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2024-05-05 21:20:23 -0700

    bugfix

--- src/pages/mensajesDirectos/mensajesDirectos.js
+++ src/pages/mensajesDirectos/mensajesDirectos.js
@@ -22,28 +22,3 @@ const listaMensajes = {
     ultimoMensaje: [""],
     horaUltimoMensaje: [""],
 }
-
-// Mensajes Directos (Emisor)
-const mensajesDirectosEmisor = {
-    idUsuario, 
-    nombreUsuario,
-    mensaje: [""],
-    horaMensaje: [""],
-}
-
-// Mensajes Directos (Receptor)
-const mensajesDirectosReceptor = {
-    idUsuario, 
-    nombreUsuario,
-    mensaje: [""],
-    horaMensaje: [""],
-}
-
-// Lista de mensajes
-const listaMensajes = {
-    idUsuario, 
-    nombreUsuario, 
-    urlImagen: [""],
-    ultimoMensaje: [""],
-    horaUltimoMensaje: [""],
-}
