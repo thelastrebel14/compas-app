33a6e8075e0b20c55f135e3a9b0f2ff4b8e4333e
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2024-05-05 20:47:05 -0700

    funciones para editar y eliminar

--- src/pages/MiEscena/miEscena.js
+++ src/pages/MiEscena/miEscena.js
@@ -276,6 +276,160 @@ function obtenerPublicaciones() {
     // });
 }
 
+// Editar publicación
+
+const formularioFotoVideo = document.querySelector('#formularioFotoVideo');
+
+formularioFotoVideo.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const datosTexto = new FormData(document.getElementById('formularioFotoVideo'));
+    var publicacionTexto = datosTexto.get('publicacionTexto');
+
+    const datosMultimedia = new FormData(document.getElementById('formularioFotoVideo'));
+    var publicacionMultimedia = datosMultimedia.get('publicacionMultimedia');
+
+    var almacenDatosFotoVideo = ('/enviarDatosFotoVideo');
+
+    fetch (almacenDatosFotoVideo, {
+        method: 'PUT',
+        body: formularioFotoVideo
+    })
+
+//     .then(data => data.json())
+//     .then(data => {
+//         if (data.success) {
+//             formularioFotoVideo.textContent = 'Publicación hecha con éxito';
+//         } else {
+//             formularioFotoVideo.textContent = 'Error al crear la publicación. Por favor inténtalo de nuevo.'
+//         }
+//     })
+
+//     .catch(error => {
+//         console.error('Error:', error);
+//         formularioFotoVideo.textContent = 'Error al crear la publicación. Por favor inténtalo de nuevo.';
+//     });
+
+});
+
+
+
+const formularioArtista = document.querySelector('#formularioArtista');
+
+formularioArtista.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const datosTituloPublicacion = new FormData(document.getElementById('formularioArtista'));
+    var tituloPublicacion = datosTituloPublicacion.get('tituloPublicacion');
+
+    const datosCuerpoPublicacion = new FormData(document.getElementById('formularioArtista'));
+    var cuerpoPublicacion = datosCuerpoPublicacion.get('cuerpoPublicacion');
+
+    const datosArtistaOEscenario = new FormData(document.getElementById('formularioArtista'));
+    var artistaOEscenario = datosArtistaOEscenario.get('artistaOEscenario');
+
+    const datosGeneroMusical = new FormData(document.getElementById('formularioArtista'));
+    var generoNorteno = datosGeneroMusical.get('generoNorteno');
+    var generoCumbia = datosGeneroMusical.get('generoCumbia');
+    var generoSalsa = datosGeneroMusical.get('generoSalsa');
+    var generoMariachi = datosGeneroMusical.get('generoMariachi');
+
+    const datosInstrumentoMusical = new FormData(document.getElementById('formularioArtista'));
+    var instrumentoGuitarra = datosInstrumentoMusical.get('instrumentoGuitarra');
+    var instrumentoAcordeon = datosInstrumentoMusical.get('instrumentoAcordeon');
+    var instrumentoBateria = datosInstrumentoMusical.get('instrumentoBateria');
+    var instrumentoSintetizador = datosInstrumentoMusical.get('instrumentoSintetizador');
+
+    const datosCodigoPostal = new FormData(document.getElementById('formularioArtista'));
+    var codigoPostal = datosCodigoPostal.get('codigoPostal');
+
+    const datosCiudadOMunicipio = new FormData(document.getElementById('formularioArtista'));
+    var ciudadOMunicipio = datosCiudadOMunicipio.get('ciudadOMunicipio');
+
+    const datosEntidadFederativa = new FormData(document.getElementById('formularioArtista'));
+    var entidadFederativa = datosEntidadFederativa.get('entidadFederativa');
+
+    const datosArtistaMultimedia = new FormData(document.getElementById('formularioArtista'));
+    var artistaMultimedia = datosArtistaMultimedia.get('artistaMultimedia');
+
+    var almacenDatosArtista = ('/enviarDatosArtista');
+
+    fetch (almacenDatosArtista, {
+        method: 'PUT',
+        body: formularioArtista
+    })
+
+    // .then(data => data.json())
+    // .then(data => {
+    //     if (data.success) {
+    //         formularioArtista.textContent = 'Publicación realizada con éxito';
+    //     } else {
+    //         formularioArtista.textContent = 'Error al crear la publicación. Por favor inténtalo de nuevo.'
+    //     }
+    // })
+
+    // .catch(error => {
+    //     console.error('Error:', error);
+    //     formularioArtista.textContent = 'Error al crear la publicación. Por favor inténtalo de nuevo.';
+    // });
+
+});
+
+// Eliminar publicación
+function eliminarPublicacion() {
+    var url = 'https://ejemploDeapi.com/eliminarPublicacion/:idPublicacion';
+    
+    publicaciones.forEach(element=> {
+        crearElementoPublicacion(element)
+    });
+
+    // Realizar la solicitud fetch
+    fetch(url, {
+        method: 'DELETE',
+        body: idPublicacion
+    })
+    .then(response => {
+        // Verificar si la respuesta fue exitosa (código de estado 200)
+        if (response.ok) {
+            // Devolver los datos en formato JSON
+            return response.json();
+        }
+        // Si la respuesta no es exitosa, lanzar un error
+        throw new Error('Error al eliminar las publicaciones');
+    })
+    .catch(error => {
+        // Manejar errores
+        console.error('Error:', error);
+    });
+}
+
+// Eliminar todas las publicaciones
+function eliminarTodasPublicaciones() {
+    var url = 'https://ejemploDeapi.com/eliminarPublicaciones';
+    
+    publicaciones.forEach(element=> {
+        crearElementoPublicacion(element)
+    });
+
+    // Realizar la solicitud fetch
+    fetch(url, {
+        method: 'DELETE',
+    })
+    .then(response => {
+        // Verificar si la respuesta fue exitosa (código de estado 200)
+        if (response.ok) {
+            // Devolver los datos en formato JSON
+            return response.json();
+        }
+        // Si la respuesta no es exitosa, lanzar un error
+        throw new Error('Error al eliminar las publicaciones');
+    })
+    .catch(error => {
+        // Manejar errores
+        console.error('Error:', error);
+    });
+}
+
 /*
     Agregar una función que haga lo siguiente:
     Se ejecute al hacer click en el botón de guardar dentro del modal de foto/video
