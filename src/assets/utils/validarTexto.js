5c313fdb7de091b118334087d0a80d56ecc0a4b1
marksalazarx <marksalazarx@gmail.com>
2024-05-11 10:16:23 -0600

    Se eliminan las alertas en función validarTexto

--- src/assets/utils/validarTexto.js
+++ src/assets/utils/validarTexto.js
@@ -1,32 +1,28 @@
 function validarTexto(texto) {
-  // Eliminar espacios en blanco antes y después del texto
-
-  // Verificar que la casilla no se deje vacía
-  if (texto == "" || texto == null) {
-    // alert("Esta casilla de texto no puede ser dejada vacía");
-    return false;
+  
+    // Eliminar espacios en blanco antes y después del texto
+      texto = texto.trim();
+  
+    // Verificar que la casilla no se deje vacía
+      if (texto == "" || texto == null) {
+        return false;
+      }
+  
+    // Verificar que el tipo de dato sea una cadena de texto
+      if(typeof(texto)!=='string') {
+          return false;
+      }
+      
+    // Veficar que los caracteres utilizados sean letras según la tabla ASCII  
+      for(var i=0;i<texto.length;i++) {
+            if(texto.charCodeAt(i) >= 65 && 
+            texto.charCodeAt(i) <= 90 && 
+            texto.charCodeAt(i) >= 97 &&
+            texto.charCodeAt(i) <= 122) {
+            }
+          }
+    
+    // Se trata de un texto sin otros caracteres
+    return true;
+  
   }
-
-  // Verificar que el tipo de dato sea una cadena de texto
-  if (typeof texto !== "string") {
-    return false;
-  }
-
-  // Verificar que los caracteres utilizados sean letras según la tabla ASCII
-  for (var i = 0; i < texto.length; i++) {
-    if (
-      texto.charCodeAt(i) >= 65 &&
-      texto.charCodeAt(i) <= 90 &&
-      texto.charCodeAt(i) >= 97 &&
-      texto.charCodeAt(i) <= 122
-    ) {
-      return true;
-    } else {
-      // alert("Por favor, introduce sólo texto en esta casilla");
-    }
-  }
-  texto = texto.trim();
-
-  // Se trata de un texto sin otros caracteres
-  return true;
-}
