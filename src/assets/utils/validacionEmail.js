bcd4f48ebc504be35fe29cd7cfd4b5a2ed9e4a4b
Patopm <egs.pato.pm@gmail.com>
2024-05-11 09:51:21 -0600

    Se corrige la funcion de email para que sea valida con validacion en vivo y se agrega el codigo de mark

--- src/assets/utils/validacionEmail.js
+++ src/assets/utils/validacionEmail.js
@@ -1,15 +1,10 @@
-const contenidoEmail = document.getElementById("submitButton");
-const errorEmail = document.getElementById("error--email");
-function validarEmail(correo)
-{
-    if (!contenidoEmail.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) 
-    {
-        errorEmail.innerHTML = "Por favor introduzca un correo valido";
-        errorEmail.style.color = "red";
-        contenidoEmail.focus.style.borderColor = "red";
-        return false;
-    }
-    errorEmail.innerHTML = "";
-    contenidoEmail.style.borderColor = "green";
-    return true;
+function validarEmail(correo) {
+  if (
+    !contenidoEmail.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
+  ) {
+    return false;
+  }
+  return true;
 }
+
+export default validarEmail;
