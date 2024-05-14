3f1ad8af818df7821b57cf24000771dc39d0ccef
marksalazarx <marksalazarx@gmail.com>
2024-05-13 20:32:26 -0600

    Actualización de diseño y función en nueva publicación

--- src/assets/utils/validarTexto.js
+++ src/assets/utils/validarTexto.js
@@ -1,3 +1,4 @@
+
 function validarTexto(texto) {
   
     // Eliminar espacios en blanco antes y después del texto
@@ -26,3 +27,64 @@ function validarTexto(texto) {
     return true;
   
   }
+
+  // funccion para validar campos en vivo
+function validacionEnVivo(inputId, callback) {
+  // recibe el id del input a validar y una funcion que returna true o false
+  let input = document.getElementById(inputId); // guarda el input dado el id
+  input.oninput = () => {
+    // cada cambio que se realiza en el input pasara por la siguiente funcion
+    let v = input.value; // guarda el valor de lo que se escribe en el input
+    callback(v); // ejecuta la funcion de validacion
+
+    // usando clases de bootstrap para validacion
+    if (callback(v)) {
+      input.classList.remove("is-invalid"); // se remueve la clase invalid
+      input.classList.add("is-valid"); // se añade la clase valid
+    } else {
+      input.classList.remove("is-valid"); // se remueve la clase valid
+      input.classList.add("is-invalid"); // se añade la clase invalid
+    }
+  };
+}
+
+/*
+ Ejemplo de uso 
+ validacionEnVivo("inputPassword",  //string del id del input
+ validaContraseña);  //callback de una funcion de validacion             
+*/
+
+validacionEnVIvo ("validacionFotoVideo01", validarTexto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
