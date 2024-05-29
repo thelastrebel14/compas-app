885cc7b506a634ee3d367cddba69ebb99ccc4065
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2024-05-28 21:14:39 -0700

    ajuste de estilos caruseles mi escena

--- src/pages/MiEscena/carrucel.js
+++ src/pages/MiEscena/carrucel.js
@@ -18,3 +18,37 @@ nextArrowEscenarios.addEventListener("click", () => {
   carouselCardsContainerEscenarios.scrollLeft += 250;
   // alert("next");
 });
+
+// ------------------Carrusel de sección de Se Busca--------------------
+const prevArrowSeBusca = document.querySelector("#carouselPrevArrowSeBusca");
+const nextArrowSeBusca = document.querySelector("#carouselNextArrowSeBusca");
+const carouselCardsContainerSeBusca = document.querySelector(
+  "#carouselCardsSeBusca"
+);
+
+prevArrowSeBusca.addEventListener("click", () => {
+  carouselCardsContainerSeBusca.scrollLeft -= 250;
+  // alert("prev");
+});
+
+nextArrowSeBusca.addEventListener("click", () => {
+  carouselCardsContainerSeBusca.scrollLeft += 250;
+  // alert("next");
+});
+
+// ------------------Carrusel de sección de Anuncios--------------------
+const prevArrowAnuncios = document.querySelector("#carouselPrevArrowAnuncios");
+const nextArrowAnuncios = document.querySelector("#carouselNextArrowAnuncios");
+const carouselCardsContainerAnuncios = document.querySelector(
+  "#carouselCardsAnuncios"
+);
+
+prevArrowAnuncios.addEventListener("click", () => {
+  carouselCardsContainerAnuncios.scrollLeft -= 250;
+  // alert("prev");
+});
+
+nextArrowAnuncios.addEventListener("click", () => {
+  carouselCardsContainerAnuncios.scrollLeft += 250;
+  // alert("next");
+});
